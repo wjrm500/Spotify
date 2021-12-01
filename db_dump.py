44ce256d4ccc6797f5be57ba69b05b5fbf5a5aff
William May <wjrm500@gmail.com>
2021-12-01 16:40:44 +0800

    Printouts

--- db_dump.py
+++ db_dump.py
@@ -7,6 +7,8 @@ from main import GetData
 command = "net start mysql801"
 os.system(command)
 
+print('Loading last 50 Spotify plays into local MySQL database...')
+
 load_dotenv()
 
 cnx = mysql.connector.connect(
@@ -25,16 +27,20 @@ cursor = cnx.cursor()
 
 a = GetData()
 play_history = a.get_play_history()
+i = 0
 for play in play_history:
     try:
         cursor.execute(add_play, play)
         cnx.commit()
+	i += 1
     except:
         pass
 
 cursor.close()
 
 cnx.close()
+
+print(f'{i} plays added!')
 
 command = "net stop mysql801"
 
