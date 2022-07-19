d4d3be25bf158239edb65617710a53192f5ec422
William May <wjrm500@gmail.com>
2022-07-19 23:05:50 +0800

    Allowing Lambda function to write to Cloudwatch log

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,23 +1,25 @@
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
+data "aws_iam_policy_document" "AWSLambdaTrustPolicy" {
+  statement {
+    actions    = ["sts:AssumeRole"]
+    effect     = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
     }
-  ]
-}
-EOF
+  }
 }
 
-resource "aws_lambda_function" "test_lambda" {
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+  assume_role_policy = "${data.aws_iam_policy_document.AWSLambdaTrustPolicy.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "terraform_lambda_policy" {
+  role       = "${aws_iam_role.iam_for_lambda.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "spotify_lambda" {
   filename         = "../test_lambda.zip"
   function_name    = "test_lambda"
   role             = aws_iam_role.iam_for_lambda.arn
@@ -31,7 +33,7 @@ resource "aws_lambda_function" "test_lambda" {
   }
 }
 
-data "archive_file" "zip_test_lambda" {
+data "archive_file" "zip_spotify_lambda" {
   type        = "zip"
   source_file = "../test_lambda.py"
   output_path = "../test_lambda.zip"
