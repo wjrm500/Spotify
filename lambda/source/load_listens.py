69cdf91931e8af07d084be6e89bc4af2fc14d57a
William May <wjrm500@gmail.com>
2022-07-29 23:22:41 +0800

    Adding create database and create table config for Spotify database

--- lambda/source/load_listens.py
+++ lambda/source/load_listens.py
@@ -11,12 +11,29 @@ def handler(event, context):
         host     = os.environ.get('DB_HOST'),
         port     = int(os.environ.get('DB_PORT'))
     )
+    create_database = "CREATE DATABASE IF NOT EXISTS spotify"
+    use_database = "USE spotify"
+    create_table = (
+        "CREATE TABLE IF NOT EXISTS spotify ("
+            "`datetime` DATETIME,"
+            "artist VARCHAR(250),"
+            "album VARCHAR(250),"
+            "song VARCHAR(250)"
+        ")"
+    )
     add_play = (
         "INSERT INTO spotify.spotify (`datetime`, artist, album, song)"
         "VALUES"
         "(%(datetime)s, %(artist)s, %(album)s, %(song)s)"
     )
     cursor = cnx.cursor()
+    print("Creating Spotify database if not exists...")
+    cursor.execute(create_database)
+    print("Switching to Spotify database...")
+    cursor.execute(use_database)
+    print("Creating Spotify table if not exists...")
+    cursor.execute(create_table)
+    cnx.commit()
     spotify = Spotify()
     play_history = spotify.get_play_history()
     i = 0
