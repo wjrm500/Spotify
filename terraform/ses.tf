d056436729c4ed09aff6595d2b4c839af6c9f5ea
William May <wjrm500@gmail.com>
2022-08-01 11:17:06 +0800

    SES Terraform (unverified)

--- terraform/ses.tf
+++ terraform/ses.tf
@@ -1,3 +1,3 @@
-# resource "aws_ses_domain_identity" "ses_domain_identity" {
-#   domain = "wjrm500@gmail.com"
-# }
+resource "aws_ses_domain_identity" "ses_domain_identity" {
+  domain = "wjrm500@gmail.com"
+}
