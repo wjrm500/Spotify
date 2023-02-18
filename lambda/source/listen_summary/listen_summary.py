2b1e5a4b000baf81d3bff0c15541dd202ba52853
William May <wjrm500@gmail.com>
2023-02-18 09:52:18 +0800

    Factoring listen summary logic into smaller functions and adding preview function for seeing email as HTML locally

--- lambda/source/listen_summary/listen_summary.py
+++ lambda/source/listen_summary/listen_summary.py
@@ -4,65 +4,88 @@ from datetime import datetime, timedelta
 import jinja2 as jj2
 import json
 import os
+from os.path import dirname, abspath
 
 BUCKET = 'wjrm500-spotify'
 OBJECT_KEY = 'play-history'
 s3 = boto3.client('s3')
 ses = boto3.client('ses')
 
-def handler(event, context):
+def get_listen_history():
     print('Getting listen history from S3...')
     obj = s3.get_object(Bucket = BUCKET, Key = OBJECT_KEY)
     listen_history = json.loads(obj['Body'].read().decode('utf-8'))
-    all_time_top_artists = Counter([x['artist'] for x in listen_history]).most_common(25)
+    return listen_history
+
+def get_all_time_top_artists(listen_history):
+    return Counter([x['artist'] for x in listen_history]).most_common(25)
+
+def get_last_week_top_artists(listen_history):
     one_week_ago = datetime.now() - timedelta(days = 7)
     last_week_listens = [x for x in listen_history if datetime.strptime(x['datetime'], '%Y-%m-%d %H:%M:%S') > one_week_ago]
-    last_week_top_artists = Counter([x['artist'] for x in last_week_listens]).most_common(25)
+    return Counter([x['artist'] for x in last_week_listens]).most_common(25)
 
-
-    email_subject = 'Your Spotify weekly digest - featuring {}, {} and {}'.format(
+def get_email_subject(last_week_top_artists):
+    return 'Your Spotify weekly digest - featuring {}, {} and {}'.format(
         last_week_top_artists[0][0],
         last_week_top_artists[1][0],
         last_week_top_artists[2][0]
     )
 
-    # Getting HTML
-    environment = jj2.Environment(loader = jj2.FileSystemLoader('.'))
+def get_html_content(last_week_top_artists, all_time_top_artists):
+    environment = jj2.Environment(loader = jj2.FileSystemLoader(dirname(abspath(__file__))))
     template = environment.get_template('email_template.html')
-    html_content = template.render(
+    return template.render(
         x = [
             ('over the past seven days', last_week_top_artists),
             ('of all time', all_time_top_artists)
         ]
     )
 
-    # Getting text
-    text_content = 'Your {} most played artists over the past week are:\n\n{}\n\nYour {} most played artists of all time are:{}'.format(
+def get_text_content(last_week_top_artists, all_time_top_artists):
+    return 'Your {} most played artists over the past week are:\n\n{}\n\nYour {} most played artists of all time are:{}'.format(
         len(last_week_top_artists),
         '\n'.join('{}: {}'.format(x[0], str(x[1])) for x in last_week_top_artists),
         len(all_time_top_artists),
         '\n'.join('{}: {}'.format(x[0], str(x[1])) for x in all_time_top_artists)
     )
 
-    resp = ses.verify_email_identity(EmailAddress = os.environ.get('email'))
-    print(resp)
-    resp = ses.send_email(
+def generate_email():
+    listen_history = get_listen_history()
+    all_time_top_artists = get_all_time_top_artists(listen_history)
+    last_week_top_artists = get_last_week_top_artists(listen_history)
+    return {
+        'subject': get_email_subject(last_week_top_artists),
+        'html_content': get_html_content(last_week_top_artists, all_time_top_artists),
+        'text_content': get_text_content(last_week_top_artists, all_time_top_artists)
+    }
+
+def preview():
+    email = generate_email()
+    with open("email.html", "w") as f:
+        f.write(email["html_content"])
+
+def handler(event, context):
+    email = generate_email()
+    ses.send_email(
         Destination = {
             'ToAddresses': [os.environ.get('email')]
         },
         Message = {
             'Subject': {
-                'Data': email_subject
+                'Data': email["subject"]
             },
             'Body': {
                 'Html': {
-                    'Data': html_content
+                    'Data': email["html_content"]
                 },
                 'Text': {
-                    'Data': text_content
+                    'Data': email["text_content"]
                 }
             }
         },
         Source = os.environ.get('email')
     )
-    print(resp)
+
+if __name__ == '__main__':
+    preview()
